db223d0784265d2f3f526d1bbc76e24c26b590dc
HanningtoneM <hanningtonepompyy@gmail.com>
2022-06-01 10:18:11 +0300

    add live demo

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   mode: 'development',
   devServer: {
-    static: './dist',
+    static: './docs',
   },
   plugins: [
     new HtmlWebpackPlugin({
