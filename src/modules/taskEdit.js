3f5012bf440e6f82b68a7f755957a7da6f096c25
HanningtoneM <hanningtonepompyy@gmail.com>
2022-06-06 11:24:42 +0300

    Fix bugs and add drag and drop

--- src/modules/taskEdit.js
+++
@@ -1,9 +0,0 @@
-export default (items) => {
-  const taskDescriptions = document.querySelectorAll('.taskText');
-  taskDescriptions.forEach((task, index) => {
-    task.addEventListener('input', () => {
-      items[index].description = task.value;
-      localStorage.localData = JSON.stringify(items);
-    });
-  });
-};
